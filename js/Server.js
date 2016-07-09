c502782a896f50cad17d9000ae3a3a2ffd059f47
Joshb421 <joshb421@gmail.com>
2016-07-09 17:06:05 +1000

    iki

--- js/Server.js
+++ js/Server.js
@@ -12,45 +12,45 @@ var options = {
     port: 3030
 };
 var client = net.connect(options, function () {
-            var socketClient = this;
-            var esp = new firmata.Board(socketClient);
-            esp.once('ready', function () {
-                    console.log('IO Ready');
-                    esp.isReady = true;
-                    var board = new five.Board({
-                        io: esp
-                        , repl: false
-                    })
-                    board.on("ready", function () {
-                            rgb = new five.Led.RGB([12, 13, 15])
-                            setInterval(function () {
-                                    console.log("calling random API")
-                                    random.generateBlobs({
-                                        size: 24
-                                        , n: 1
-                                        , format: string
-                                    }).then(function (result) {
-                                            console.log(result.random.data); // [55, 3]
-                                            rgb.color(result.random.data)
-                                            console.log(bintohex(result.random.data)
-                                            });
-                                    }, 5000)
-                            });
-                    }); //startup code here
-            });
-        // Other functions in this section
-        function bintohex(mybin) {
-            mybin = document.getElementById('bin').value;
-            z = -1;
-            number = 0;
-            for (i = mybin.length; i > -1; i--) {
-                //Every 1 in binary string is converted to decimal and added to number
-                if (mybin.charAt(i) == "1") {
-                    number += Math.pow(2, z);
-                }
-                z += 1;
-            }
-            // Return is converting decimal to hexadecimal
-            mybin = number.toString(16);
-            return mybin
+    var socketClient = this;
+    var esp = new firmata.Board(socketClient);
+    esp.once('ready', function () {
+        console.log('IO Ready');
+        esp.isReady = true;
+        var board = new five.Board({
+            io: esp
+            , repl: false
+        })
+        board.on("ready", function () {
+            rgb = new five.Led.RGB([12, 13, 15])
+            setInterval(function () {
+                console.log("calling random API")
+                random.generateBlobs({
+                    size: 24
+                    , n: 1
+                    , format: string
+                }).then(function (result) {
+                    console.log(result.random.data); // [55, 3]
+                    rgb.color(result.random.data)
+                    console.log(bintohex(result.random.data));
+                });
+            }, 5000)
+        });
+    }); //startup code here
+});
+// Other functions in this section
+function bintohex(mybin) {
+    mybin = document.getElementById('bin').value;
+    z = -1;
+    number = 0;
+    for (i = mybin.length; i > -1; i--) {
+        //Every 1 in binary string is converted to decimal and added to number
+        if (mybin.charAt(i) == "1") {
+            number += Math.pow(2, z);
         }
+        z += 1;
+    }
+    // Return is converting decimal to hexadecimal
+    mybin = number.toString(16);
+    return mybin
+}
