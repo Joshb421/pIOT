6719d22521e1d4ba90d99d9548000220225bd568
Joshb421 <joshb421@gmail.com>
2016-08-02 19:11:41 +1000

    Added natural wake up - To be tested

--- js/Server.js
+++ js/Server.js
@@ -53,7 +53,7 @@ var client = net.connect(options, function () {
             status = new five.Led(14);
             status.on();
             rgb = new five.Led.RGB([12, 13, 15]);
-            wakeUp([true, true, true, true, true, true, true], 18, d.getMinutes() + 15)
+            wakeUp([true, true, true, true, true, true, true], 19, d.getHours() + 10)
                 //                    setInterval(function () {
                 //                        var delay = greenDelay
                 //                        setInterval(function () {
