675901e0add3f093a5dfb60fba0759418f634761
Josh Bywater <joshb421@gmail.com>
2016-08-14 13:09:58 +1000

    h

--- js/Server.js
+++ js/Server.js
@@ -119,6 +119,7 @@ function RGBStrip(mode, time, hex, brightness) {
     if (mode == 1) {
         RGB = [255, 0, 0];
         for (RGB[1]; RGB[1] < 256; RGB[1]++) {
+            console.log(RGB[1])
             setTimeout(function () {
                 console.log(RGB);
             }, time / 6 / 255);
